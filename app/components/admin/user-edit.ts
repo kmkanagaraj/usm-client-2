670122ccadc5b5f4e41681bc6fc778f9a66095bc
kamlesh <kaverma@redhat.com>
2016-01-13 05:09:54 +0530

    Manage password enhancements
    verify the old password while updating the password.
    if user's old password does not match with given old password ,
    than error message will appear . and also matching main password and confirm password.
    
    Change-Id: I3950a631631c99d77051ff0137c092008180ded3
    Signed-off-by: kamlesh <kaverma@redhat.com>

--- app/components/admin/user-edit.ts
+++ app/components/admin/user-edit.ts
@@ -7,8 +7,11 @@ export class UserEditController {
     private lastName:string;
     private email:string;
     private password:string;
+    private confirmpassword:string;
+    private oldpassword:string;
     private status:boolean;
     private notificationenabled:boolean;
+    private errorMessage:string;
 
     static $inject: Array<string> = [
         '$location',
@@ -41,17 +44,25 @@ export class UserEditController {
     }
 
     public saveSettings():void {
-        var setting = {
-                password: this.password
-        };
-        this.UserService.saveUserSetting(this.userId,setting).then((result) => {
-            if(result.status === 200){
-
-            }
-            else{
-                alert("cannot update");
-            }
-        });
+        this.errorMessage = "";
+        if(this.password === this.confirmpassword && this.password.length > 0) {
+            var setting = {
+                    password: this.password,
+                    oldpassword: this.oldpassword
+            };
+            this.UserService.saveUserSetting(this.userId,setting).then((result) => {
+                if(result.status === 200){
+                   var responseMsg = JSON.parse(result.data);
+                   if(JSON.parse(responseMsg).message === 'password-changed') {
+                     this.errorMessage = "password changed successfully";
+                   }else {
+                      this.errorMessage = JSON.parse(responseMsg).message;
+                   }
+                }
+            });
+        }else {
+            this.errorMessage = "Confirm password does not match!";
+        }
     }
 
     public save():void {
