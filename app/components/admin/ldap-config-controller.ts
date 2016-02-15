b4ecad9240e9a1fa63200e9b659a1d9030878a01
Karnan <kchidamb@redhat.com>
2016-02-15 17:43:42 +0530

    Enables Ldap configuration through UI
    users can configure the ldap connection settings
    through Ldap config page
    
    Change-Id: If7ba71687b0a313a8b3974bfdfa0ae30a1b50a3e
    Signed-off-by: Karnan <kchidamb@redhat.com>

---
+++ app/components/admin/ldap-config-controller.ts
@@ -0,0 +1,53 @@
+import {LdapService} from '../rest/ldap';
+
+export class LdapConfigController {
+    private errorMsg: boolean;
+    private ldapserver : string;
+    private port: number;
+    private base: string;
+    private domainadmin: string;
+    private password:string;
+    
+    static $inject: Array<string> = [
+        '$location',
+        'LdapService',
+    ];
+
+    constructor(
+        private $location: ng.ILocationService,
+        private LdapService: LdapService) {
+            this.getConfig();
+    }
+
+    public save():void {
+        var config = {
+            ldapserver: this.ldapserver,
+            port: this.port,
+            base: this.base,
+            domainadmin: this.domainadmin,
+            password: this.password,
+            uid: "cn",
+            firstname: "cn",
+            lastname: "sn",
+            mail: "mail"
+        };
+
+        this.LdapService.saveLdapConfig(config).then((result) => {
+            if(result.status === 200) {
+                this.errorMsg = false;
+            }else{
+                this.errorMsg = true;
+            }
+        });
+     }
+     
+     public getConfig(){
+         this.LdapService.getLdapConfig().then((config)=>{
+            this.ldapserver = config.ldapserver,
+            this.port = config.port,
+            this.base =  config.base,
+            this.domainadmin = config.domainadmin
+        });
+     }
+
+}
