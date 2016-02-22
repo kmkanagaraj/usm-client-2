03ce962cb45cf7f9be0eec40357d26a9432db999
Karnan <kchidamb@redhat.com>
2016-02-22 15:53:27 +0530

    Refractor events and tasks
    Moved existing tasks pages under /tasks and introduced events.
    
    Change-Id: I27cc0f0e5511dbaf13151da4642b4ff902de29e0
    Signed-off-by: Karnan <kchidamb@redhat.com>

--- app/components/tasks/task-list.ts
+++ app/components/tasks/task-list.ts
@@ -1,10 +1,10 @@
 // <reference path="../../../typings/tsd.d.ts" />
 
 import {UtilService} from '../rest/util';
-import {EventService} from '../rest/events';
+import {RequestService} from '../rest/request';
 import {RequestTrackingService} from '../requests/request-tracking-svc';
 
-export class EventListController {
+export class TaskListController {
     private list: Array<any>;
     private timer;
     private pageNo = 1;
@@ -15,7 +15,6 @@ export class EventListController {
         '$interval',
         '$location',
         '$q',
-        'EventService',
         'RequestService'
     ];
     constructor(
@@ -23,7 +22,7 @@ export class EventListController {
         private $interval: ng.IIntervalService,
         private $location: ng.ILocationService,
         private $q: ng.IQService,
-        private eventSvc: EventService) {
+        private requestSvc: RequestService) {
         this.timer = this.$interval(() => this.refresh(), 5000);
         this.$scope.$on('$destroy', () => {
             this.$interval.cancel(this.timer);
@@ -32,7 +31,7 @@ export class EventListController {
     }
 
     public refresh() {
-        this.eventSvc.getList(this.pageNo,this.pageSize).then((data :any) => {
+        this.requestSvc.getList(this.pageNo,this.pageSize).then((data :any) => {
             this.totalPages = Math.ceil(data.totalcount/this.pageSize);
             this.loadData(data.tasks);
         });
@@ -45,22 +44,31 @@ export class EventListController {
         this.refresh();
     }
 
-    public loadData(events: Array<any>) {
+    public loadData(tasks: Array<any>) {
         var list = [];
-        _.each(events, (event) => {
-            if (event.parentid === '00000000-0000-0000-0000-000000000000') {
-                var lastStatus = _.last<any>(event.statuslist);
+        _.each(tasks, (task) => {
+            if (task.parentid === '00000000-0000-0000-0000-000000000000') {
+                var lastStatus = _.last<any>(task.statuslist);
                 if (lastStatus) {
-                    event.statusMsg = lastStatus.Message;
-                    event.timestamp = lastStatus.Timestamp;
+                    task.statusMsg = lastStatus.Message;
+                    task.timestamp = lastStatus.Timestamp;
                 }
-                list.push(event)
+                if ((!task.completed) && task.subtasks.length > 0) {
+                    this.requestSvc.get(task.subtasks[0]).then((subtask) => {
+                        var lastStatus = _.last<any>(subtask.statuslist);
+                        if (lastStatus) {
+                            task.statusMsg = lastStatus.Message;
+                            task.timestamp = lastStatus.Timestamp;
+                        }
+                    });
+                }
+                list.push(task)
             }
         });
         this.list = list.reverse();
     }
 
-    public viewDetails(eventId) {
-        this.$location.path('/events/' + eventId);
+    public viewDetails(taskId) {
+        this.$location.path('/tasks/' + taskId);
     }
 }
