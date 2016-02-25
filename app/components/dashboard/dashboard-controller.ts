ba1f445c325cb5da76093ebea0f7f002e92ef781
kamlesh <kaverma@redhat.com>
2016-02-26 02:19:34 +0530

    code optimization for main dashboard
    done with some code optimization and type specific checking
    
    Change-Id: I8d1e660a0437ff7e059dd7d179c74b2907b7b83b
    Signed-off-by: kamlesh <kaverma@redhat.com>

--- app/components/dashboard/dashboard-controller.ts
+++ app/components/dashboard/dashboard-controller.ts
@@ -1,7 +1,10 @@
-import {ClusterService} from '../rest/clusters';
 import {ServerService} from '../rest/server';
-import {NodeStatus} from '../rest/server';
 import {numeral} from '../base/libs';
+
+interface UsageData {
+    used: number;
+    total: number;
+};
 
 export class DashboardController {
     private clusters: any;
@@ -19,14 +22,12 @@ export class DashboardController {
         '$scope',
         '$location',
         '$log',
-        'ClusterService',
         'ServerService'
     ];
 
     constructor(private $scope: ng.IScope,
         private $location: ng.ILocationService,
         private $log: ng.ILogService,
-        private clusterService: ClusterService,
         private serverService: ServerService) {
 
          this.utilization = { data: {}, config: {} };
@@ -40,7 +41,6 @@ export class DashboardController {
          this.monitors = { total: 0, warning: 0, critical: 0 };
          this.capacity = {};
          this.serverService.getDashboardSummary().then((summary) => this.loadDashboardData(summary));
-         this.serverService.getList().then((nodes) => this.loadMonitors(nodes));
     }
 
     /**
@@ -51,56 +51,8 @@ export class DashboardController {
             this.$location.path('/first');
         }
         else {
-            //overall utilization data
-            this.capacity.total = numeral(summary.usage.total).format('0 b');
-            this.capacity.used = numeral(summary.usage.used).format('0 b');
-            this.utilization.data.total = summary.usage.total;
-            this.utilization.data.used = summary.usage.used;
-            this.utilization.config.chartId = "utilizationChart";
-            this.utilization.config.thresholds = {'warning':'60','error':'90'};
-            this.utilization.config.legend = {"show":false};
-            this.utilization.config.tooltipFn = (d) => {
-                  return '<span class="donut-tooltip-pf"style="white-space: nowrap;">' +
-                           numeral(d[0].value).format('0 b') + ' ' + d[0].name +
-                         '</span>';
-            };
-            this.utilization.config.centerLabelFn = () => {
-                  return Math.round(100 * (this.utilization.data.used / this.utilization.data.total)) + "% Used";
-            };
-
-
-            //storage utilization by profile
-            this.utilizationByProfile.title = 'Utilization by storage profile';
-            this.utilizationByProfile.layout = {
-              'type': 'multidata'
-            };
-            var profiles = summary.storageprofileusage;
-            var subdata = [];
-            var othersProfile = { "used": 0, "total": 0};
-            for (var profile in profiles) {
-                if (profiles.hasOwnProperty(profile)) {
-                    var usedData = Math.round(100 * (profiles[profile]["used"] / profiles[profile]["total"]));
-                    if(profile === 'general') {
-                        subdata.push({ "used" : usedData , "color" : "#004368" , "subtitle" : "General" });
-                    }else if(profile === 'sas') {
-                        subdata.push({ "used" : usedData , "color" : "#00659c" , "subtitle" : "SAS" });
-                    }else if(profile === 'ssd') {
-                        subdata.push({ "used" : usedData , "color" : "#39a5dc" , "subtitle" : "SSD" });
-                    }else{
-                        othersProfile.used = othersProfile.used + profiles[profile]["used"];
-                        othersProfile.total = othersProfile.total + profiles[profile]["total"];
-                    }
-                }
-            }
-            var othersProfilePercent = Math.round(100 * (othersProfile.used / othersProfile.total));
-            if (othersProfilePercent > 0) {
-                subdata.push({ "used" : othersProfilePercent , "color" : "#7dc3e8" , "subtitle" : "Others" });
-            }
-            this.utilizationByProfile.data = {
-              'total': '100',
-              'subdata' : subdata
-            };
-
+            this.formatlUtilizationData(summary.usage);
+            this.formatlUtilizationByProfileData(summary.storageprofileusage);
             this.objects.total = summary.objectcnt;
             this.osds.total = summary.slucount.total;
             this.hosts.total = summary.nodescount.total;
@@ -108,21 +60,66 @@ export class DashboardController {
             this.hosts.unaccepted = summary.nodescount.unaccepted;
             this.clusters.total = summary.clusterscount.total;
             this.clusters.critical = summary.clusterscount.error;
+            this.pools.total = summary.storagecount.total;
+            this.pools.critical = summary.storagecount.down;
+            this.monitors.total = summary.others.ceph.monitor;
         }
     }
 
     /**
-     *This is the callback function called after getting monitors list.
+     *This is the helper function for format the overall utilization data.
     */
-    public loadMonitors(nodes: Array<any>) {
-        _.each(nodes, (node: any) => {
-            if (node.options1.mon === 'Y') {
-                this.monitors.total++;
-                if(node.status === NodeStatus.ERROR) {
-                    this.monitors.critical++;
+    public formatlUtilizationData(usage: UsageData) {
+        this.capacity.total = numeral(usage.total).format('0 b');
+        this.capacity.used = numeral(usage.used).format('0 b');
+        this.utilization.data.total = usage.total;
+        this.utilization.data.used = usage.used;
+        this.utilization.config.chartId = "utilizationChart";
+        this.utilization.config.thresholds = {'warning':'60','error':'90'};
+        this.utilization.config.legend = {"show":false};
+        this.utilization.config.centerLabelFn = () => {
+              return Math.round(100 * (this.utilization.data.used / this.utilization.data.total)) + "% Used";
+        };
+        this.utilization.config.tooltipFn = (d) => {
+              return '<span class="donut-tooltip-pf"style="white-space: nowrap;">' +
+                       numeral(d[0].value).format('0 b') + ' ' + d[0].name +
+                     '</span>';
+        };
+    }
+
+    /**
+     *This is the helper function for format the utilization by profile data.
+    */
+    public formatlUtilizationByProfileData(profiles: any) {
+        this.utilizationByProfile.title = 'Utilization by storage profile';
+        this.utilizationByProfile.layout = {
+          'type': 'multidata'
+        };
+        var subdata = [];
+        var othersProfile: UsageData = { "used": 0, "total": 0};
+        for (var profile in profiles) {
+            if (profiles.hasOwnProperty(profile)) {
+                var usedData = Math.round(100 * (profiles[profile]["used"] / profiles[profile]["total"]));
+                if(profile === 'general') {
+                    subdata.push({ "used" : usedData , "color" : "#004368" , "subtitle" : "General" });
+                }else if(profile === 'sas') {
+                    subdata.push({ "used" : usedData , "color" : "#00659c" , "subtitle" : "SAS" });
+                }else if(profile === 'ssd') {
+                    subdata.push({ "used" : usedData , "color" : "#39a5dc" , "subtitle" : "SSD" });
+                }else{
+                    othersProfile.used = othersProfile.used + profiles[profile]["used"];
+                    othersProfile.total = othersProfile.total + profiles[profile]["total"];
                 }
             }
-        });
+        }
+        var othersProfilePercent = Math.round(100 * (othersProfile.used / othersProfile.total));
+        if (othersProfilePercent > 0) {
+            subdata.push({ "used" : othersProfilePercent , "color" : "#7dc3e8" , "subtitle" : "Others" });
+        }
+        this.utilizationByProfile.data = {
+          'total': '100',
+          'subdata' : subdata
+        };
     }
 
 }
