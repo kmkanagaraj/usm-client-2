d7295665439ea4ac49a85a4a5c55d5521aa82d25
Kanagaraj M <kmayilsa@redhat.com>
2016-02-29 09:55:39 +0530

    listing available block devices
    A new sub menu 'RBDs' is added to the 'Storage' menu.
    On clicking this, list of block devices will be shown
    with their respective size.
    
    Change-Id: Iadd9b0617384c31beed7efa37fba9ee17762e5b8
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

---
+++ app/components/storage/blockdevice/blockdevice-list.ts
@@ -0,0 +1,54 @@
+// <reference path="../../../typings/tsd.d.ts" />
+
+import {BlockDeviceService} from '../../rest/blockdevice';
+import {numeral} from '../../base/libs';
+
+export class BlockDeviceListController {
+    private list: Array<any>;
+    private clusterMap = {};
+    private timer;
+    static $inject: Array<string> = [
+        '$scope',
+        '$interval',
+        '$location',
+        '$log',
+        '$timeout',
+        '$q',
+        'BlockDeviceService'
+    ];
+    constructor(
+        private $scope: ng.IScope,
+        private $interval: ng.IIntervalService,
+        private $location: ng.ILocationService,
+        private $log: ng.ILogService,
+        private $timeout: ng.ITimeoutService,
+        private $q: ng.IQService,
+        private blockDeviceSvc: BlockDeviceService) {
+        //this.timer = this.$interval(() => this.refresh(), 5000);
+        this.$scope.$on('$destroy', () => {
+            this.$interval.cancel(this.timer);
+        });
+        this.refresh();
+    }
+
+    public refresh() {
+        this.blockDeviceSvc.getList().then(blockdevices => {
+            this.loadData(blockdevices);
+        });
+    }
+
+    public loadData(blockdevices) {
+        this.list = blockdevices;
+    }
+
+    public getFormatedSize(size: number): string {
+        return numeral(size).format('0 b');
+    }
+
+    public create() {
+        this.$location.path('/storage/new');
+    }
+
+    public remove(rbd) {
+    }
+}
