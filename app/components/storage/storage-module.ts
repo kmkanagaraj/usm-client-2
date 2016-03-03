d6efc87ed7c3cced0c17773ea90a5a14b0c531d3
Kanagaraj M <kmayilsa@redhat.com>
2016-03-03 14:48:03 +0530

    adding block-device-item directive
    Converted the block device item view to an angular
    directive to make it available for re-use.
    
    Change-Id: Iacda6161a3d4e59874ac927a0298cef8670bd093
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

--- app/components/storage/storage-module.ts
+++ app/components/storage/storage-module.ts
@@ -6,12 +6,14 @@ import {StorageNewController} from './storage-new';
 import {ObjectStorageController} from './object/storage-new-object';
 import {BlockDeviceController} from './blockdevice/blockdevice-new';
 import {BlockDeviceListController} from './blockdevice/blockdevice-list';
+import {BlockDeviceItem} from './blockdevice/blockdevice-item';
 
 var angular: ng.IAngularStatic = require('angular');
 
 var moduleName = 'kitoon.storage';
 
 angular.module(moduleName, ['rzModule'])
+    .directive('blockDeviceItem', () => new BlockDeviceItem())
     .controller('StorageListController', StorageListController)
     .controller('StorageNewController', StorageNewController)
     .controller('ObjectStorageController', ObjectStorageController)
