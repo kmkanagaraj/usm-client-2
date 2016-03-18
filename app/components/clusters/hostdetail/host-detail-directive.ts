a8a8ff97c93ab7ab3c2e45ee2d8eb2022996d637
kamleshKumarVerma <kaverma@redhat.com>
2016-03-18 18:07:45 +0530

    cluster dashboard - added host detail page
    in cluster dashboard , added host detail page
    
    Change-Id: I0753825ea22bea6c11f7a8a54cfe9474a03bd2d5
    Signed-off-by: kamleshKumarVerma <kaverma@redhat.com>

---
+++ app/components/clusters/hostdetail/host-detail-directive.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+
+/*
+ * @ngdoc directive
+ * @name cluster:clusterHostDetail
+ * @scope
+ * @restrict E
+ *
+ * @description
+ * An AngularJS directive for showing the details of hosts in cluster.
+ *
+ * @example
+ * <cluster-host-detail cluster-id="clusterid"></cluster-host-detail>
+ *
+*/
+
+import {ClusterHostDetailController} from './host-detail';
+
+export class ClusterHostDetail implements ng.IDirective {
+    restrict: string = "E";
+    scope = {
+        id: '=clusterId'
+    };
+    controllerAs: string = 'hosts';
+    bindToController: boolean = true;
+    controller = ClusterHostDetailController;
+    templateUrl = 'views/hosts/hosts.html';
+}
