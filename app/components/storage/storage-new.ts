166232e0e4b21fcc6a6d76b81becb9433c50836c
DhivyaSiva <dhivyasivaprakasam9@gmail.com>
2016-04-05 16:35:57 +0530

    Kitoon : Addstorage wizard cannot be started with zero nodes.
    When a Create Cluster task fails and the cluster ends up
    with 0 nodes, then the user unable to start "Add Storage" Wizard.
    
    Change-Id: If9cd595901ae3f22537291995c45fa8f65d136a7
    Signed-off-by: DhivyaSiva <dhivyasivaprakasam9@gmail.com>

--- app/components/storage/storage-new.ts
+++ app/components/storage/storage-new.ts
@@ -6,6 +6,7 @@ import {Cluster} from '../rest/clusters';
 export class StorageNewController {
     private clusters: Array<Cluster>;
     private cluster: Cluster;
+    private canCreateStorage: number;
     private type: String;
 
     static $inject: Array<string> = [
@@ -20,9 +21,28 @@ export class StorageNewController {
             this.clusters = clusters;
             if (this.clusters.length > 0) {
                 this.cluster = this.clusters[0];
+                this.getClusterSummary(this.cluster);
             }
         });
         this.type = "object";
+    }
+
+    public getClusterSummary(cluster):void {
+        this.clusterSvc.get(cluster.clusterid).then((result) => {
+            if(result.state == 2) {
+                 return this.clusterSvc.getClusterSummary(cluster.clusterid);
+            }
+            else {
+                this.canCreateStorage = 0;
+            }
+        }).then((summary) => {
+            if(summary.slucount.total) {
+                this.canCreateStorage = 2;
+            }
+            else {
+                this.canCreateStorage = 1;
+            }
+        });
     }
 
     public next(): void {
