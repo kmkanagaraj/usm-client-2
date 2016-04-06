fc88abe9aa447c4d43f2a24350105088f74e323b
Ramesh Nachimuthu <rnachimu@redhat.com>
2016-04-06 11:25:16 +0530

    <WIP>Import ceph cluster
      Add UI for importing a Ceph cluster. User has to select
    a node (Ceph Monitor) from the available hosts list. It can
    be an unaccepted host or an already accepted host. If it is
    unaccepted then backend will automatically accept the host and
    get the Ceph cluster information from the ceph monitor.
    
      Cluster informations will be shown to the user. If there are
    any nodes in the Ceph cluster which are not known to Skyring
    then error will be shown. User has to configure the node so that
    it is reporting to skyring. Once user verifies all the
    information in the cluster then he can summit the import
    cluster request.
    
    Change-Id: Ie5fe721fe9b2bc0cfed3261f218fec5ef16155a4
    Signed-off-by: Ramesh Nachimuthu <rnachimu@redhat.com>

--- app/components/rest/clusters.ts
+++ app/components/rest/clusters.ts
@@ -215,4 +215,14 @@ export class ClusterService {
         });
     }
 
+    // **getImportClusterDeatils
+    // **@returns** A promise with Ceph Cluster Information for importing
+    getImportClusterDeatils(monNode: string) {
+        return this.restFull.one('/importclusternodes?bootstrapnode=' + monNode + "&clustertype=ceph").get();
+    }
+
+    importCephCluster(bootStrapInfo: any) {
+        return this.restFull.all('importcluster').post(bootStrapInfo);
+    }
+
 }
