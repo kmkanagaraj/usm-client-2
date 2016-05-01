82b2b3dc133d06416f876b298991bdbf34ce9764
kamleshKumarVerma <kaverma@redhat.com>
2016-05-01 09:40:47 +0530

    added host osd detail page
    implemented osd detail tab in host detail page
    
    Change-Id: Ib720c805d519654f6b5470267160cf7f0463cce2
    Signed-off-by: kamleshKumarVerma <kaverma@redhat.com>

--- app/components/hosts/host-module.ts
+++ app/components/hosts/host-module.ts
@@ -5,6 +5,7 @@ import {HostListController} from './host';
 import {HostDetailController} from './host-details/host-detail';
 import {HostOverview} from "./host-overview/host-overview-directive";
 import {HostConfig} from "./host-config/host-config-directive";
+import {HostOsd} from "./host-osd/host-osd-directive";
 import {TimeSlot} from "./time-slot/time-slot-directive";
 
 var angular: ng.IAngularStatic = require('angular');
@@ -16,6 +17,7 @@ angular.module(moduleName, [])
     .controller('HostDetailController', HostDetailController)
     .directive('hostOverview', () => new HostOverview())
     .directive('hostConfig', () => new HostConfig())
+    .directive('hostOsd', () => new HostOsd())
     .directive('timeSlot', () => new TimeSlot());
-    
+
 export default moduleName;
